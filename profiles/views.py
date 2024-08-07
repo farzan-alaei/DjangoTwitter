9dbc1800be2764edd061219b363aeca05e2e42bc
farzan <farzan.70@gmial.com>
2024-08-07 10:02:02 +0330

    add forms for profiles app

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views.generic.edit import FormView
+
+from profiles.forms import ProfileForm
+
 
 # Create your views here.
+
+
+class ProfileView(FormView):
+    template_name = "profile.html"
+    form_class = ProfileForm
+    success_url = "/"
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
