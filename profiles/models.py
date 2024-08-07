bf47c4c74d8a2b45601cc4791239b943152a243b
farzan <farzan.70@gmial.com>
2024-08-07 09:35:58 +0330

    add requirements

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    """
+    TwitterProfile model
+    """
+
+    user = models.OneToOneField(
+        "auth.User",
+        on_delete=models.CASCADE,
+        related_name="twitter_profile",
+    )
+    avatar = models.ImageField(upload_to="avatars/", blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}"
